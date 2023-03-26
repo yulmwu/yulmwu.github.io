8d6bf73ee9dd0794bfa36a6856241a43239fb805
ky0422 <ky0422a@gmail.com>
2023-03-26 20:14:01 +0900

    `update`: updated `checkout.sh`

--- checkout.sh
+++ checkout.sh
@@ -5,6 +5,6 @@ GREP_PROMPT=$(grep -r '> {: .prompt-*' ./_posts ./_tabs)
 if [[ -n "$GREP_PROMPT" ]]; then
     echo "Checkout: Found invalid prompt in the following files:\n"
     echo "$GREP_PROMPT" | while read -r line; do
-        echo ${line/:> {: .prompt-* }}
+        echo $line
     done
 fi
