27e6fce3878eafa0a118a840e179107e5cb1f259
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-07-21 00:16:40 +0900

    updated

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -3,7 +3,7 @@ import { CardContainerContent } from '../components/home/CardContainer'
 export const certifications: CardContainerContent[] = [
     {
         logoColor: '#91c5ff',
-        title: '정보처리기사',
+        title: '정보처리기능사',
         description: '한국산업인력공단',
         period: '2023.09',
     },
--- src/data/histories.ts
+++ src/data/histories.ts
@@ -5,13 +5,13 @@ export const histories: CardContainerContent[] = [
         logoColor: '#9CDCFF',
         title: 'Null4U',
         description: '세명컴퓨터고등학교 보안과 클라우드 전공동아리',
-        period: '2025.03',
+        period: '2025.03 ~',
     },
     {
         logo: 'https://i.namu.wiki/i/V5BiwATJt7DFQHl1Y07cKPd0rskt-ffPrHoQu4JDdBPmGXHO5JYXSWOqDp81t6xgkYEI7xzjTpWfi2P8VXcPfSKLBehqy58v7Cwx33RXWdXC_xxh8NqXPEb9YYTGYeOQ3WHZE-ou2X9SxWK8g6bcyob1UagUTc2JzuvI5W_UGDk.webp',
         title: '세명컴퓨터고등학교',
         description: '스마트보안솔루션과 11기',
-        period: '2024.03',
+        period: '2024.03 ~',
     },
     {
         logoColor: '#9DE3A4',
